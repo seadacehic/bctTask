5eaffdf2626557dc5b580e2972c44a2ee3c65cb0
Seada Čehić <seada.cehic@edu.fit.ba>
2020-07-24 09:57:11 +0200

    Implemented multiplayer game result calculation.

--- Controllers/BoggleController.cs
+++ Controllers/BoggleController.cs
@@ -24,8 +24,34 @@ namespace bctTask.Controllers
             return calculatedScore;
         }
 
+        //Multiplayer game result calculation
+        [HttpPost("[action]")]
+        public IEnumerable<Player> CalculateMultiplayerBoggleResult([FromBody] Player[] players)
+        {
+            List<Player> playerResults = new List<Player>(players);
+            List<string> allWords = BoggleHelper.getAllWords(playerResults);
 
-        
+            for (int i = 0; i < playerResults.Count; i++)
+            {
+                int calculatedScore = 0;
+                string[] uniqueWords = playerResults[i].Words.Distinct().ToArray();
+
+                for (int j = 0; j < uniqueWords.Length; j++)
+                {
+                    //Calculates points only for the unique words
+                    if (allWords.Where(x => x == uniqueWords[j]).Count() == 1)
+                    {
+                        calculatedScore += BoggleHelper.getPointsByWord(uniqueWords[j]);
+                    }
+                }
+
+                playerResults[i].Result = calculatedScore;
+            }
+
+            return playerResults;
+        }
+
+
     }
 }
 
