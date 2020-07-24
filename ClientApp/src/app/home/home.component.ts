5493478440a6ea056d2c813eebb4d0003f23806f
Seada Čehić <seada.cehic@edu.fit.ba>
2020-07-24 15:30:17 +0200

    Connected UI with API and implemented a timer.

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -10,12 +10,19 @@ export class HomeComponent {
   public letters: object[];
   public words: string[];
   public word: string;
+  public result: number;
+  public http: HttpClient;
+  public baseUrl: string;
+  public counter: any;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-   // this.letters = this.generateRandomLetters();
     this.generateRandomLetters();
+    this.initilizeCounter();
     this.words = [];
     this.word = "";
+    this.http = http;
+    this.baseUrl = baseUrl;
+    this.counter = null;
   }
   public addLetter(value, id) {
     console.log('letter: ', value);
@@ -55,6 +62,9 @@ export class HomeComponent {
 
     this.word = '';
     this.words = [];
+    this.result = -1;
+    this.initilizeCounter();
+    this.generateRandomLetters();
   }
 
   public saveWord() {
@@ -78,5 +88,41 @@ export class HomeComponent {
     }
 
     this.word = '';
+  }
+
+  public finishGame() {
+      this.http.post<number>(this.baseUrl + 'api/Boggle/CalculateBoggleResult', this.words).subscribe(result => {
+        this.result = result;
+      }, error => console.error(error));
+  }
+
+
+  //counter logic copied from https://www.w3schools.com/howto/howto_js_countdown.asp
+  public initilizeCounter() {
+    // Set the date we're counting down to
+    var countDownDate = new Date(Date.now() + 3000 * 60);
+    var me = this;
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      document.getElementById("timer").innerHTML = "Time left: " + minutes + "m " + seconds + "s ";
+
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        me.finishGame();
+        document.getElementById("timer").innerHTML = "Time expired!";
+      }
+    }, 1000);
+
   }
 }
